3f22638fb020b970777a1ae458e34e2ffc8ebcf5
programming6131 <kimutainashon@gmail.com>
2024-08-13 20:08:54 +0300

    next state fn

--- main.rs
+++ main.rs
@@ -7,4 +7,17 @@ enum Event {
     Coin,
 }
 
+fn next_state(state: State, event: Event) -> State {
+    match state {
+        State::Locked => match event {
+            Event::Push => State::Locked,
+            Event::Coin => State::UnLocked,
+        },
+        State::UnLocked => match event {
+            Event::Push => State::Locked,
+            Event::Coin => State::UnLocked,
+        },
+    }
+}
+
 fn main() {}
